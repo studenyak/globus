536b71c1cb46460b4d57d53b7331845e12c9476d
Alex Studenyak <alexst@similarweb.com>
2018-07-18 18:06:29 +0300

    Added script to get worldwide data

--- data_generator.py
+++ data_generator.py
@@ -1,10 +1,24 @@
+# This script gets data from the FlickrAPI server and builds data structures that we need
+# {lon, lat, total}
+# Depending on the zoomLevel the data is stored to the specific Redis db
+# db=0 - World wide
+# db=1
+# ...
+# db=20 - Street wide
+
 import flickr_api
+import logging
 
-EARTH_RADIUS_KM = 6371
-EARTH_SQUARE_KM2 = 510100000
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-# Square of a cell that we will use to go over earth square
-MAX_CELL_SQUARE_KM2 = 1024
 
-def get(radius):
-    pass
+def get_worldwide_data():
+    for lon in range (-180, -175):
+        for lat in range(-90, -85):
+            res = flickr_api.get_photos_by_bbox([lon, lat, lon + 1, lat + 1])
+            obj = dict()
+            obj['lon'] = lon
+            obj['lat'] = lat
+            obj['weight'] = res['photos']['total']
+            logging.info(obj)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import tests
 import utils
 import redis
 import json
+import data_generator
 
 redis_config = {'host': 'localhost',
                 'port': 6379,
@@ -37,26 +38,27 @@ def dump_redis_to_json(db=0):
 if __name__ == "__main__":
     # dump_redis_to_json()
 
-    area = tests.get_orvietto_area()
-    response = flickr_api.get_photos_by_bbox([area['start']['lon'], area['start']['lat'],
-                                              area['end']['lon'], area['end']['lat']])
-
-    total = response['photos']['total']
-    perpage = response['photos']['perpage']
-    photo = response['photos']['photo']
-
-    for index in range(0, perpage):
-        info = flickr_api.get_photo_info(photo[index]['id'])
-        key = str(info['photo']['location']['latitude']+','+info['photo']['location']['longitude'])
-        value = rDb.get(key)
-        if not value:
-            value = dict()
-            value['weight'] = 1
-            # value['photo'] = [info]
-        else:
-            value = json.loads(value)
-            value['weight'] += 1
-            # value['photo'].insert(int(value['weight']) - 1, info)
-        rDb.set(key, json.dumps(value))
+    data_generator.get_worldwide_data()
+    # area = tests.get_orvietto_area()
+    # response = flickr_api.get_photos_by_bbox([area['start']['lon'], area['start']['lat'],
+    #                                           area['end']['lon'], area['end']['lat']])
+    #
+    # total = response['photos']['total']
+    # perpage = response['photos']['perpage']
+    # photo = response['photos']['photo']
+    #
+    # for index in range(0, perpage):
+    #     info = flickr_api.get_photo_info(photo[index]['id'])
+    #     key = str(info['photo']['location']['latitude']+','+info['photo']['location']['longitude'])
+    #     value = rDb.get(key)
+    #     if not value:
+    #         value = dict()
+    #         value['weight'] = 1
+    #         # value['photo'] = [info]
+    #     else:
+    #         value = json.loads(value)
+    #         value['weight'] += 1
+    #         # value['photo'].insert(int(value['weight']) - 1, info)
+    #     rDb.set(key, json.dumps(value))
 
 
